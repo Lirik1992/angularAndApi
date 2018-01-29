ef540766a2e7a4e92ba5c27c01959b571f4fad63
Dima Likharev <ldo1992@list.ru>
2018-01-29 17:28:24 +0300

    Little fix

--- app/js/scrollReveal.js
+++ app/js/scrollReveal.js
@@ -1,30 +1,36 @@
 window.sr = ScrollReveal();
 sr.reveal('.showcase-left', {
   duration: 2000,
+  reset: true,
   origin: 'left',
   distance: '100px'
 });
 sr.reveal('.showcase-right', {
   duration: 2000,
+  reset: true,
   origin: 'right',
   distance: '100px'
 });
 sr.reveal('.showcase-btn', {
   duration: 2000,
+  reset: true,
   delay: 1000,
   origin: 'bottom'
 });
 sr.reveal('.inlineimg', {
   duration: 2000,
+  reset: true,
   origin: 'top'
 });
 sr.reveal('.imageInfo', {
   duration: 3000,
+  reset: true,
   distance: '200px',
   origin: 'bottom'
 });
 sr.reveal('.feedback', {
   duration: 2000,
   distance: '80px',
+  reset: true,
   origin: 'bottom'
 });
