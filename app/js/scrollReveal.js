98d0aeccec61df0ca23e99ba246d27f92d25ef63
Dima Likharev <ldo1992@list.ru>
2018-01-29 17:16:44 +0300

    Paralax combined with scrollreveal

---
+++ app/js/scrollReveal.js
@@ -0,0 +1,30 @@
+window.sr = ScrollReveal();
+sr.reveal('.showcase-left', {
+  duration: 2000,
+  origin: 'left',
+  distance: '100px'
+});
+sr.reveal('.showcase-right', {
+  duration: 2000,
+  origin: 'right',
+  distance: '100px'
+});
+sr.reveal('.showcase-btn', {
+  duration: 2000,
+  delay: 1000,
+  origin: 'bottom'
+});
+sr.reveal('.inlineimg', {
+  duration: 2000,
+  origin: 'top'
+});
+sr.reveal('.imageInfo', {
+  duration: 3000,
+  distance: '200px',
+  origin: 'bottom'
+});
+sr.reveal('.feedback', {
+  duration: 2000,
+  distance: '80px',
+  origin: 'bottom'
+});
